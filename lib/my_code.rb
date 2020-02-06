f677f1ba2a154b1d03847647f7f81503021c6160
Daniel Succar <danielsuccar@icloud.com>
2020-02-06 21:45:18 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,27 @@
-# Your Code Here
+require 'pry'
+
+def map(array)
+  new = []
+  i=0
+  while i < array.length do
+    new.push(yield(array[i]))
+    i += 1
+  end
+  new
+end
+
+def reduce(array, sp = nil)
+  if sp
+    sum = sp
+    i = 0
+  else
+    sum = array[0]
+    i=1
+  end
+  while i < array.length do
+    sum = yield(sum,array[i])
+    i += 1
+    binding.pry
+  end
+  sum
+  end
