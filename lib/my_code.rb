acf320dbf0427cd47b82e7f86df5a0c40904c66c
Dawit Gizaw <gizawdawit123@gmail.com>
2020-02-13 03:59:58 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,27 @@
 # Your Code Here
+def map(source_array)
+  index = 0
+  new = []
+  while index < source_array.length do
+    new.push(yield(source_array[index]))
+    index += 1
+  end
+  new
+end
+
+def reduce(source_array, starting_point = nil)
+
+  if starting_point
+    i = 0
+    sum = starting_point
+  else
+    sum = source_array[0]
+    i = 1
+  end
+  
+  while i < source_array.length do
+    sum = yield(sum, source_array[i])
+    i += 1
+  end
+  sum
+end
