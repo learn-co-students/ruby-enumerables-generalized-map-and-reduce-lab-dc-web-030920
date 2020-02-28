7d4538c7c03a5e71a83ff7f85ad3f62c95fdb204
Bret Gibson <bret.gibson@gmail.com>
2020-02-28 05:38:33 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,32 @@
-# Your Code Here
+def map(src)
+  i = 0
+  newArr = []
+  while i < src.count do
+    newArr.push(yield(src[i]))
+    i+=1
+  end
+  newArr
+end
+
+#(map([1, 2, 3, -9]){|n| n * -1})
+
+def reduce(src, starting_point = 0)
+  if starting_point
+    newVal = starting_point
+    i = 0
+  else
+    newVal = src[0]
+    i = 1
+  end
+  while i < src.count do
+    newVal = yield(newVal, src[i])
+    if src[i]
+      newVal = true
+    else
+      newVal = false
+      return newVal
+    end
+    i+=1
+  end
+  return newVal
+end
